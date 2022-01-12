6dba0e34b682e6cddf6b5c2dd18150f769240398
German <german.s.1993@yandex.ru>
2022-01-12 22:32:35 +0300

    fixed

--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,29 @@
 let editButton = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
 let closeButton = document.querySelector('.popup__close');
-let form = document.querySelector('.popup__container');
+let form = document.querySelector('.popup__form');
 let nameInput = form.querySelector('#name');
 let jobInput = form.querySelector('#job');
 let profileName = document.querySelector('.profile__name');
 let job = document.querySelector('.profile__description');
-let likeButtons = document.querySelectorAll('.photos__like-button');
 
 function popupClose() {
   popup.classList.remove('popup_opened');
 }
 
-editButton.addEventListener('click', function() {
+function popupOpen() {
   popup.classList.add('popup_opened');
   nameInput.value = profileName.textContent;
   jobInput.value = job.textContent;
-})
-closeButton.addEventListener('click', popupClose)
-form.addEventListener('submit', function(evt) {
+}
+
+function formSubmitHandler(evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   job.textContent = jobInput.value;
   popupClose();
-})
-
-for (let i=0; i<likeButtons.length; i++) {
-  let likeButton = likeButtons[i];
-  likeButtons[i].addEventListener('click', function() {
-    likeButton.classList.toggle('photos__like-button_active');
-  })
 }
+
+editButton.addEventListener('click', popupOpen);
+closeButton.addEventListener('click', popupClose);
+form.addEventListener('submit', formSubmitHandler);
